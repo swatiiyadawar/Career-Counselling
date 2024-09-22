9e89eda04d1d08932d26c02eef9acb92266b191c
vitthal-07 <vitthalbiradar50@gmail.com>
2024-09-22 14:05:17 +0530

    change import path for all imports in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
 // src/App.jsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Resource from './components/Resources';
 import Homepage from './components/Homepage';
-import SignIn from './components/SignIn'; // Fixed import path
+import SignIn from './components/SignIn';
 import Pathways from './components/Pathways';
 import Chat from './components/Chat';
 import Test from './components/Test';
-import Tools from './components/tools';
-import Dashboard from './components/Dashboard';
+import Tools from './components/Tools';
 import Mentorship from './components/Mentorship';
+import Dashboard from './components/Dashboard';
 import Chatbot from './components/chat-comp/Chatbot';
-import Footer from './components/Footer';
-import Resource from './components/Resources';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path='/pathways' element={<Pathways/>} />
-        <Route path='/chat' element={<Chat/>} />
-        <Route path='/resources' element={<Resource/>} />
-        <Route path='/tests' element={<Test/>} />
-        <Route path='/tools' element={<Tools/>} />
-        <Route path='/mentor' element={<Mentorship/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-      </Routes>
-      <Chatbot />
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/pathways" element={<Pathways />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/resources" element={<Resource />} />
+                <Route path="/tests" element={<Test />} />
+                <Route path="/tools" element={<Tools />} />
+                <Route path="/mentor" element={<Mentorship />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+            <Chatbot />
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
