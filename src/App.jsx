f8443ef6b8161d3b3f40107057c2f60e876d2ae6
vitthal-07 <vitthalbiradar50@gmail.com>
2024-09-22 14:03:14 +0530

    change import path for resource

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 // src/App.jsx
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import SignIn from './components/SignIn'; // Fixed import path
 import Pathways from './components/Pathways';
 import Chat from './components/Chat';
-import Resource from './components/resources';
 import Test from './components/Test';
 import Tools from './components/tools';
 import Dashboard from './components/Dashboard';
 import Mentorship from './components/Mentorship';
 import Chatbot from './components/chat-comp/Chatbot';
 import Footer from './components/Footer';
+import Resource from './components/Resources';
 
 function App() {
   return (
